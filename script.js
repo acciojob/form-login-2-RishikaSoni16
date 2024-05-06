f4380d46b640d6c92e2bc7fc1f2a05242c91fb15
RishikaSoni16 <135811075+RishikaSoni16@users.noreply.github.com>
2024-05-06 22:40:34 +0600

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the submit button element
+let sub = document.getElementById("submit1");
+sub.addEventListener("click",()=>{
+    let name=document.getElementById('fname').value;
+    let last=document.getElementById('lname').value;
+    let phone=document.getElementById('Pno').value;
+    let email=document.getElementById('email').value;
+    let message = 'First Name: ' + name + 
+    ' Last Name: ' + last +
+    ' Phone Number: ' + phone + 
+    ' Email ID: ' + email;
+    console.log(message);
+    alert(message);
+});
+});
